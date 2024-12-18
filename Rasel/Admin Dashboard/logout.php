9c6df2793dd037fe104f8208652d2a365463b4ee
Md Rasel Bhuyan <mdrasel9724@gmail.com>
2024-12-18 20:55:52 +0600

    some changes

--- Rasel/Admin Dashboard/logout.php
+++ Rasel/Admin Dashboard/logout.php
@@ -4,6 +4,6 @@ session_start();
 session_destroy();  // End the session
 
 // Redirect to login page or display a message
-header("Location: login.php");
+header("Location: ../../index.php");
 exit();
 ?>
